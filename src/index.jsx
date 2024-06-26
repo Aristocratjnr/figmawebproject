90eaa512e58ac236a3327102714a689e5122bf86
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-26 00:57:50 +0000

    refactor: Remove unnecessary code and update file structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleFontLoader from './GoogleFontLoader';
-import './reset.css'; 
+import './reset.css';
 import './index.css'; 
 import './global.css'; 
 
