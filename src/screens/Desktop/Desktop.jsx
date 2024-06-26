a457fc45fc7784aac3bf92e8b804b73ea53c8561
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-26 08:32:53 +0000

    refactor: Optimize Desktop component rendering and remove unused CSS imports

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="frame">
+          <div className="overlap-group">
+            <div className="ellipse" />
+            <img
+              className="ph-heart-fill"
+              alt="Ph heart fill"
+              src="https://c.animaapp.com/Zo7kpJCa/img/ph-heart-fill.svg"
+            />
+          </div>
+        </div>
+        <div className="text-wrapper">Tibet Five set Couch</div>
+        <p className="p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
+          odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          Curabitur tempus urna at turpis condimentum lobortis.
+        </p>
+        <div className="overlap">
+          <div className="text-wrapper-2">Sackln Cloth</div>
+          <div className="text-wrapper-3">Type of Materials Used</div>
+        </div>
+        <div className="overlap-2">
+          <div className="text-wrapper-2">2 Weeks</div>
+          <p className="text-wrapper-3">Estimated Time To Be Worked On</p>
+        </div>
+        <div className="overlap-3">
+          <div className="text-wrapper-4">Single</div>
+          <div className="text-wrapper-5">Double</div>
+          <div className="text-wrapper-6">Triple</div>
+          <div className="text-wrapper-3">Size</div>
+        </div>
+        <div className="text-wrapper-7">Overview</div>
+        <div className="text-wrapper-8">Available Colors</div>
+        <div className="frame-2" />
+        <div className="frame-3" />
+        <div className="frame-4" />
+        <div className="frame-5" />
+        <div className="div-wrapper">
+          <div className="text-wrapper-9">Buy</div>
+        </div>
+        <div className="frame-6">
+          <div className="text-wrapper-10">Pre-order</div>
+        </div>
+        <div className="ellipse-2" />
+        <div className="ellipse-3" />
+        <div className="ellipse-4" />
+        <div className="ellipse-5" />
+      </div>
+    </div>
+  );
+};
