ce715fab4a9c13f479afa4bb6869a91eced456a5
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-26 08:30:48 +0000

    refactor: Update React rendering to use createRoot and render Desktop component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import GoogleFontLoader from './GoogleFontLoader';
-import './reset.css';
-import './index.css';
+import "../global.css";
+import "../global.css";
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import { Desktop } from "./screens/Desktop";
 
-const  Index = document.getElementById('root');
-console.log(Index); 
-const root = createRoot(Index);
-
-root.render(
-  <>
-    <GoogleFontLoader />
-    <App />
-  </>
-);
-export default Index;
+const app = document.getElementById("app");
+const root = ReactDOMClient.createRoot(app);
+root.render(<Desktop />);
