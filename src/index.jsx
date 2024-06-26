0370abc9f3717928f68f2d5d49cf243d9e006b07
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-26 00:01:59 +0000

    chore: Add global.css for global styles

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import GoogleFontLoader from './GoogleFontLoader';
 import './reset.css'; // Assuming the reset styles are saved as reset.css
 import './index.css'; // Any custom styles
+import './global.css'; // Any global styles
 
 const App = () => {
   return (
