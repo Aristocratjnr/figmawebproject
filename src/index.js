df741d8bba294e5d56280091dfb883497889be1f
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-26 00:53:30 +0000

    refactor: Remove unnecessary code and update file structure

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // assuming the above code is saved as App.jsx or App.js
-import './index.css'; // any custom styles
-
-ReactDOM.render(<App />, document.getElementById('root'));
