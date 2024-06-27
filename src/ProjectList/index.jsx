dee64ffb25ddcf43d23efdb17dc0b2686c90293f
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-27 11:13:47 +0000

    added projectlist

---
+++ src/ProjectList/index.jsx
@@ -0,0 +1,341 @@
+import React from "react";
+import "./style.css";
+
+export const ProductListingK = () => {
+  return (
+    <div className="product-listing-k">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="overlap-2">
+              <img className="unsplash" alt="Unsplash" src="unsplash-eksdfrxs28s.png" />
+              <div className="frame" />
+              <div className="overlap-wrapper">
+                <div className="overlap-3">
+                  <img className="img" alt="Frame" src="frame-65.svg" />
+                  <img className="img" alt="Frame" src="frame-65.svg" />
+                  <img className="img" alt="Frame" src="frame-65.svg" />
+                  <div className="text-wrapper">TWO SET KITCHEN CABNET</div>
+                  <div className="frame-2">
+                    <div className="text-wrapper-2">2wks</div>
+                    <div className="overlap-group-2">
+                      <div className="text-wrapper-3">105</div>
+                      <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                    </div>
+                    <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                    <div className="ellipse" />
+                    <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="overlap-group-wrapper">
+                <div className="overlap-4">
+                  <img className="ph-heart-light" alt="Ph heart light" src="ph-heart-light.svg" />
+                  <div className="overlap-5">
+                    <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+                    <div className="frame-3">
+                      <div className="text-wrapper-2">2wks</div>
+                      <div className="overlap-group-2">
+                        <div className="text-wrapper-3">105</div>
+                        <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                      </div>
+                      <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                      <div className="ellipse-2" />
+                      <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="frame-wrapper">
+              <div className="div-wrapper">
+                <div className="overlap-6">
+                  <img className="ph-heart-light" alt="Ph heart light" src="ph-heart-light.svg" />
+                  <div className="overlap-5">
+                    <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+                    <div className="frame-3">
+                      <div className="text-wrapper-2">2wks</div>
+                      <div className="overlap-group-2">
+                        <div className="text-wrapper-3">105</div>
+                        <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                      </div>
+                      <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                      <div className="ellipse-3" />
+                      <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-4">
+            <div className="frame-5">
+              <div className="overlap-7">
+                <img className="ph-heart-light-2" alt="Ph heart light" src="ph-heart-light.svg" />
+                <div className="overlap-8">
+                  <div className="text-wrapper-5">TWO SET KITCHEN CABNET</div>
+                  <div className="frame-3">
+                    <div className="text-wrapper-2">2wks</div>
+                    <div className="overlap-group-2">
+                      <div className="text-wrapper-3">105</div>
+                      <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                    </div>
+                    <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                    <div className="ellipse-4" />
+                    <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-9">
+          <div className="frame-6">
+            <div className="overlap-6">
+              <div className="overlap-5">
+                <div className="text-wrapper-6">TWO SET KITCHEN CABNET</div>
+                <div className="frame-3">
+                  <div className="text-wrapper-2">2wks</div>
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-3">105</div>
+                    <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                  </div>
+                  <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                  <div className="ellipse-5" />
+                  <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                </div>
+              </div>
+              <img className="ph-heart-light-3" alt="Ph heart light" src="ph-heart-light.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-7">
+          <div className="overlap-group-3">
+            <div className="KITCHEN-LIVING-ROOM">
+              KITCHEN&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;LIVING
+              ROOM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DINNING&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BEDROOM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BATHROOM
+            </div>
+            <img className="line" alt="Line" src="line-2.svg" />
+          </div>
+        </div>
+        <div className="frame-8">
+          <div className="overlap-10">
+            <img className="frame-9" alt="Frame" src="frame-65.svg" />
+            <div className="text-wrapper-7">TWO SET KITCHEN CABNET</div>
+            <div className="frame-10">
+              <div className="text-wrapper-2">2wks</div>
+              <div className="overlap-group-2">
+                <div className="text-wrapper-3">105</div>
+                <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+              </div>
+              <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+              <div className="ellipse-6" />
+              <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-11">
+          <div className="frame-12">
+            <img className="ph-heart-light-4" alt="Ph heart light" src="ph-heart-light.svg" />
+            <div className="overlap-11">
+              <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+              <div className="frame-3">
+                <div className="text-wrapper-2">2wks</div>
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-3">105</div>
+                  <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                </div>
+                <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                <div className="ellipse-7" />
+                <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-12">
+          <div className="frame-13">
+            <div className="overlap-13">
+              <p className="TWO-SET-KITCHEN">
+                TWO SET KITCHEN CABNET
+                <br />
+                DURATION TO BE ASSEMBLED : 2WEEKS
+              </p>
+              <div className="frame-14" />
+            </div>
+          </div>
+          <div className="frame-15">
+            <div className="overlap-14">
+              <img className="ph-heart-light-5" alt="Ph heart light" src="ph-heart-light.svg" />
+              <div className="overlap-5">
+                <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+                <div className="frame-3">
+                  <div className="text-wrapper-2">2wks</div>
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-3">105</div>
+                    <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                  </div>
+                  <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                  <div className="ellipse-8" />
+                  <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-15">
+          <div className="frame-16">
+            <div className="TWO-SET-KITCHEN-wrapper">
+              <p className="p">
+                TWO SET KITCHEN CABNET
+                <br />
+                DURATION TO BE ASSEMBLED : 2WEEKS
+              </p>
+            </div>
+          </div>
+          <div className="frame-17">
+            <div className="overlap-11">
+              <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+              <div className="frame-3">
+                <div className="text-wrapper-2">2wks</div>
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-3">105</div>
+                  <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                </div>
+                <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                <div className="ellipse-9" />
+                <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+              </div>
+            </div>
+            <img className="ph-heart-fill" alt="Ph heart fill" src="ph-heart-fill.svg" />
+          </div>
+        </div>
+        <div className="frame-18">
+          <div className="frame-19">
+            <img className="ph-heart-light-6" alt="Ph heart light" src="ph-heart-light.svg" />
+            <div className="overlap-11">
+              <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+              <div className="frame-3">
+                <div className="text-wrapper-2">2wks</div>
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-3">105</div>
+                  <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                </div>
+                <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                <div className="ellipse-10" />
+                <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-20">
+          <img className="ph-heart-light-7" alt="Ph heart light" src="ph-heart-light.svg" />
+          <div className="overlap-11">
+            <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+            <div className="frame-3">
+              <div className="text-wrapper-2">2wks</div>
+              <div className="overlap-group-2">
+                <div className="text-wrapper-3">105</div>
+                <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+              </div>
+              <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+              <div className="ellipse-10" />
+              <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-21">
+          <img className="line-2" alt="Line" src="line-10.svg" />
+          <img className="ph-meta-logo-thin" alt="Ph meta logo thin" src="ph-meta-logo-thin.svg" />
+          <img className="frame-22" alt="Frame" src="frame-9.svg" />
+          <div className="frame-23">
+            <img className="solar-copyright" alt="Solar copyright" src="solar-copyright-outline.svg" />
+            <p className="text-wrapper-8">Copyright 2024, All Right Reserved</p>
+          </div>
+          <div className="privacy-policy-terms">
+            Privacy
+            Policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Terms
+            &amp; Conditions
+          </div>
+        </div>
+        <div className="frame-24">
+          <div className="frame-12">
+            <img className="ph-heart-light-4" alt="Ph heart light" src="ph-heart-light.svg" />
+            <div className="overlap-11">
+              <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+              <div className="frame-3">
+                <div className="text-wrapper-2">2wks</div>
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-3">105</div>
+                  <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                </div>
+                <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                <div className="ellipse-11" />
+                <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-25">
+          <div className="frame-12">
+            <img className="ph-heart-light-4" alt="Ph heart light" src="ph-heart-light.svg" />
+            <div className="overlap-11">
+              <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+              <div className="frame-3">
+                <div className="text-wrapper-2">2wks</div>
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-3">105</div>
+                  <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                </div>
+                <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                <div className="ellipse-12" />
+                <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-26">
+          <div className="overlap-16">
+            <div className="frame-27">
+              <img className="ph-heart-light-8" alt="Ph heart light" src="ph-heart-light.svg" />
+              <div className="overlap-11">
+                <div className="text-wrapper-4">TWO SET KITCHEN CABNET</div>
+                <div className="frame-3">
+                  <div className="text-wrapper-2">2wks</div>
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-3">105</div>
+                    <img className="tabler-meter-square" alt="Tabler meter square" src="tabler-meter-square.svg" />
+                  </div>
+                  <img className="tabler-arrows" alt="Tabler arrows" src="tabler-arrows-maximize.svg" />
+                  <div className="ellipse-13" />
+                  <img className="game-icons-duration" alt="Game icons duration" src="game-icons-duration.svg" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-28" />
+        <div className="overlap-17">
+          <div className="ABOUT">
+            <div className="text-wrapper-9">Kitchen</div>
+          </div>
+          <div className="nave">
+            <img className="ph-meta-logo-thin-2" alt="Ph meta logo thin" src="ph-meta-logo-thin.svg" />
+            <div className="pages">
+              <div className="HOME-ABOUT-SERVICES">
+                HOME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ABOUT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SERVICES&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;COLLECTION&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CONTACT
+              </div>
+            </div>
+            <img className="line-3" alt="Line" src="line-2.svg" />
+            <div className="frame-29">
+              <img className="iconamoon-search" alt="Iconamoon search" src="iconamoon-search-thin.svg" />
+              <p className="text-wrapper-10">What are you looking for?</p>
+            </div>
+            <img className="icon-park-outline" alt="Icon park outline" src="icon-park-outline-love-and-help.svg" />
+            <img className="line-4" alt="Line" src="line-1.svg" />
+            <img className="iconamoon-profile" alt="Iconamoon profile" src="iconamoon-profile-thin.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
