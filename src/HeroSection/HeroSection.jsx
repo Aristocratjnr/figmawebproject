5c3306064fefcdb35b357bbf7f02f28aeeb43f5d
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-27 11:09:32 +0000

    herosection file added with its components

---
+++ src/HeroSection/HeroSection.jsx
@@ -0,0 +1,285 @@
+import React from "react";
+import "./style.css";
+
+export const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="div">
+        <div className="nave">
+          <img className="ph-meta-logo-thin" alt="Ph meta logo thin" src="ph-meta-logo-thin.svg" />
+          <div className="pages">
+            <div className="HOME-ABOUT-SERVICES">
+              HOME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ABOUT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SERVICES&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;COLLECTION&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CONTACT
+            </div>
+          </div>
+          <img className="line" alt="Line" src="line-2.svg" />
+          <div className="frame">
+            <img className="iconamoon-search" alt="Iconamoon search" src="iconamoon-search-thin.svg" />
+            <p className="text-wrapper">What are you looking for?</p>
+          </div>
+          <img className="icon-park-outline" alt="Icon park outline" src="icon-park-outline-love-and-help.svg" />
+          <img className="img" alt="Line" src="line-1.svg" />
+          <img className="iconamoon-profile" alt="Iconamoon profile" src="iconamoon-profile-thin.svg" />
+        </div>
+        <div className="HOME">
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">Pre-order Instead</div>
+          </div>
+          <div className="frame-2">
+            <div className="text-wrapper-2">Shop Ready Stock</div>
+          </div>
+          <div className="overlap-group">
+            <div className="text-wrapper-3">Kayujati</div>
+            <div className="where-comfort-meets">
+              Where Comfort <br />
+              Meets Craftsmanship
+            </div>
+            <div className="text-wrapper-4">For Urban Living</div>
+            <img className="line-2" alt="Line" src="line-5.svg" />
+          </div>
+        </div>
+        <div className="ABOUT">
+          <div className="overlap">
+            <div className="frame-3" />
+            <div className="frame-4" />
+          </div>
+          <div className="overlap-2">
+            <div className="text-wrapper-5">WHO WE ARE</div>
+            <p className="qorem-ipsum-dolor">
+              Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac
+              aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent&nbsp;&nbsp;conubia nostra, per
+              inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut
+              diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
+            </p>
+          </div>
+          <div className="text-wrapper-6">15</div>
+          <div className="text-wrapper-7">64</div>
+          <div className="text-wrapper-8">200</div>
+          <div className="text-wrapper-9">1079</div>
+          <div className="years-experience">
+            Years
+            <br />
+            experience
+          </div>
+          <div className="awards-gained">
+            Awards
+            <br />
+            gained
+          </div>
+          <div className="people-trained">
+            People
+            <br />
+            trained
+          </div>
+          <div className="products-sold">
+            Products
+            <br />
+            sold
+          </div>
+        </div>
+        <div className="ABOUT-2">
+          <div className="OUR-GALLERY">
+            OUR <br />
+            GALLERY
+          </div>
+          <img className="ri-circle-fill" alt="Ri circle fill" src="image.svg" />
+          <img className="ri-circle-fill-2" alt="Ri circle fill" src="ri-circle-fill.svg" />
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-3">
+            <div className="overlap-4">
+              <p className="p">EXPERIENCE THE DIFFERENCE WITH US</p>
+              <div className="frame-5" />
+            </div>
+            <div className="element-free-consultation">01.&nbsp;&nbsp;Free Consultation</div>
+            <img className="line-3" alt="Line" src="line-3.svg" />
+            <img className="material-symbols" alt="Material symbols" src="material-symbols-light-arrow-insert.svg" />
+            <div className="element-custom-furniture">02.&nbsp;&nbsp;Custom Furniture</div>
+            <div className="element-furniture">03.&nbsp;&nbsp;Furniture Assembling</div>
+            <div className="element-training-sessions">04.&nbsp;&nbsp;Training Sessions</div>
+            <div className="element-free-delivery">05.&nbsp;&nbsp;Free Delivery</div>
+            <img className="line-4" alt="Line" src="line-3.svg" />
+            <img className="line-5" alt="Line" src="line-3.svg" />
+            <img className="line-6" alt="Line" src="line-3.svg" />
+            <img className="line-7" alt="Line" src="line-3.svg" />
+            <img className="material-symbols-2" alt="Material symbols" src="material-symbols-light-arrow-insert.svg" />
+            <img className="material-symbols-3" alt="Material symbols" src="material-symbols-light-arrow-insert.svg" />
+            <img className="material-symbols-4" alt="Material symbols" src="material-symbols-light-arrow-insert.svg" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <div className="frame-6" />
+                <img className="vector" alt="Vector" src="vector.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="ABOUT-3">
+          <div className="what-can-we-do">
+            What can <br />
+            we do.
+          </div>
+          <p className="text-wrapper-10">
+            Jorem ipsum dolor sit amet,stra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+            lobortis.
+          </p>
+          <p className="text-wrapper-11">
+            Jorem ipsum dolor sit amet,stra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+            lobortis.
+          </p>
+          <p className="text-wrapper-12">
+            Jorem ipsum dolor sit amet,stra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+            lobortis.
+          </p>
+          <p className="text-wrapper-13">
+            Jorem ipsum dolor sit amet,stra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+            lobortis.
+          </p>
+          <img className="line-8" alt="Line" src="line-3.svg" />
+          <img className="line-9" alt="Line" src="line-4.svg" />
+          <img className="line-10" alt="Line" src="line-4.svg" />
+          <div className="text-wrapper-14">DESIGNING</div>
+          <div className="text-wrapper-15">CREATING</div>
+          <div className="text-wrapper-16">FINISHING</div>
+          <img className="mingcute-finger" alt="Mingcute finger" src="mingcute-finger-press-line.svg" />
+          <img className="mage-stars-c" alt="Mage stars c" src="mage-stars-c.svg" />
+          <p className="text-wrapper-17">
+            Jorem ipsum dolor sit amet,stra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+            lobortis.
+          </p>
+          <div className="text-wrapper-18">LAYOUTING</div>
+          <img className="ph-hammer-thin" alt="Ph hammer thin" src="ph-hammer-thin.svg" />
+          <img className="ph-hands-clapping" alt="Ph hands clapping" src="ph-hands-clapping-thin.svg" />
+          <div className="frame-7">
+            <div className="text-wrapper-19">Contact Us</div>
+          </div>
+        </div>
+        <div className="overlap-5">
+          <div className="frame-8">
+            <div className="overlap-6">
+              <div className="text-wrapper-20">WHAT OTHERS ARE SAYING</div>
+              <div className="overlap-7">
+                <div className="text-wrapper-21">“</div>
+                <p className="text-wrapper-22">
+                  Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac
+                  aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                  himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam
+                  quam, semper iaculis condimentum ac, vestibulum eu nisl.
+                </p>
+                <img className="eos-icons-three-dots" alt="Eos icons three dots" />
+              </div>
+            </div>
+            <div className="text-wrapper-23">Radans</div>
+          </div>
+          <img className="ph-dots-three" alt="Ph dots three" src="ph-dots-three-outline-thin.svg" />
+        </div>
+        <div className="frame-9">
+          <img className="line-11" alt="Line" src="line-10.svg" />
+          <img className="ph-meta-logo-thin-2" alt="Ph meta logo thin" src="ph-meta-logo-thin.svg" />
+          <img className="frame-10" alt="Frame" src="frame-9.svg" />
+          <div className="frame-11">
+            <img className="solar-copyright" alt="Solar copyright" src="solar-copyright-outline.svg" />
+            <p className="text-wrapper-24">Copyright 2024, All Right Reserved</p>
+          </div>
+          <div className="privacy-policy-terms">
+            Privacy
+            Policy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Terms
+            &amp; Conditions
+          </div>
+        </div>
+        <div className="FA-qs">
+          <div className="overlap-8">
+            <div className="text-wrapper-25">FAQs</div>
+            <div className="sign-in-circle-light">
+              <div className="overlap-group-3">
+                <img className="ellipse" alt="Ellipse" src="ellipse-47.svg" />
+                <img className="vector-2" alt="Vector" src="vector-9.svg" />
+              </div>
+            </div>
+            <p className="text-wrapper-26">Do you offer delivery services?</p>
+          </div>
+          <div className="add-duotone-line">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-2">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-3">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-4">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-5">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-6">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-7">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-8">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-9">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <div className="add-duotone-line-10">
+            <div className="overlap-9">
+              <img className="vector-3" alt="Vector" src="vector-52.svg" />
+              <img className="vector-4" alt="Vector" src="vector-53.svg" />
+            </div>
+          </div>
+          <p className="text-wrapper-27">Do you offer custom closet design services?</p>
+          <p className="text-wrapper-28">What financing options do you offer?</p>
+          <p className="text-wrapper-29">What financing options do you offer?</p>
+          <p className="text-wrapper-30">What is your warranty policy goods and services?</p>
+          <p className="text-wrapper-31">What is your warranty policy goods and services?</p>
+          <p className="text-wrapper-32">Do you offer any closet lighting solutions and installation services?</p>
+          <p className="text-wrapper-33">Do you offer any closet lighting solutions and installation services?</p>
+          <p className="text-wrapper-34">How long does the typical closet installation take?</p>
+          <p className="text-wrapper-35">Do you offer delivery services?</p>
+          <p className="text-wrapper-36">What types of closet systems do you offer?</p>
+          <img className="line-12" alt="Line" src="line-19.svg" />
+          <img className="line-13" alt="Line" src="line-19.svg" />
+          <img className="line-14" alt="Line" src="line-19.svg" />
+          <img className="line-15" alt="Line" src="line-19.svg" />
+          <img className="line-16" alt="Line" src="line-19.svg" />
+          <img className="line-17" alt="Line" src="line-19.svg" />
+          <img className="line-18" alt="Line" src="line-19.svg" />
+          <img className="line-19" alt="Line" src="line-19.svg" />
+          <img className="line-20" alt="Line" src="line-19.svg" />
+          <img className="line-21" alt="Line" src="line-19.svg" />
+          <img className="line-22" alt="Line" src="line-19.svg" />
+        </div>
+      </div>
+    </div>
+  );
+};
