f54a48a588fb6259b2ae7e1d690fad40d959d3b6
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-30 22:23:50 +0000

    refactor: Update rendering method in Index.jsx and add HeroSection component

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,10 +1,13 @@
 import "../global.css";
 import "../global.css";
+import ""
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { Desktop } from "./screens/Desktop";
+import { HeroSection } from "./HeroSection/HeroSection";
 
 const app = document.getElementById("app");
 const root = ReactDOMClient.createRoot(app);
 root.render
-(<Desktop />);
+(<><Desktop /><HeroSection /></>
+);
