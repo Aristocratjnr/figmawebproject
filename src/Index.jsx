7ea84ba480763a95cdd908aa0d6c18a2c32f1c60
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-01 19:56:47 +0000

    refactor: Update HeroSection assets URLs and improve rendering

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,14 +1,10 @@
-import "../global.css";
-import "../global.css";
+
 import "../src/HeroSection/assets/css/style.css";
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import { Desktop } from "./screens/Desktop";
 import { HeroSection } from "./HeroSection/HeroSection";
-import { ProductListingK } from "./ProjectList";
-
 const app = document.getElementById("app");
 const root = ReactDOMClient.createRoot(app);
 root.render
-(<><HeroSection /></>
+(<HeroSection />
 );
