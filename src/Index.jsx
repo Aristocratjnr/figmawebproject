eb6ae3c689743f3fabf4ae1d54990569161b288c
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-02 00:49:16 +0000

    refactor: Remove unused assets and update rendering in HeroSection

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,6 +1,5 @@
 import "../global.css";
 import "../global.css";
-import "../src/HeroSection/assets/css/style.css";
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { Desktop } from "./screens/Desktop";
